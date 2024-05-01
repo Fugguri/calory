49c959cafe6d2488fc6df29fae4813c85f7cd5aa
Fugguri <fygguri@gmail.com>
2024-05-01 15:07:50 +0300

    update

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -11,9 +11,8 @@ class Keyboards:
         self.start_cd = CallbackData("start", "character_id")
         self.admin_cd = CallbackData("mailing", "command")
         self.mailing_cd = CallbackData("admin", "command")
-        self.add_calory_diary_cd = CallbackData(
-            "calory", 'd', "cal", "pr", 'gr', "carbs", 'fats'
-        )
+        self.add_calory_diary_cd = CallbackData('d'
+                                                )
         self.add_dish_to_error_list_cd = CallbackData("dish", "amount")
         self.confirm_promo_cd = CallbackData("promo", "code", "percent")
         self.confirm_bill_cb = CallbackData(
@@ -74,11 +73,11 @@ class Keyboards:
         kb.add(InlineKeyboardButton(text="Добавить запись",
                callback_data=self.add_calory_diary_cd.new(
                    d=food_data.dish,
-                   cal=food_data.calories,
-                   pr=food_data.protein,
-                   gr=food_data.grams,
-                   carbs=food_data.carbs,
-                   fats=food_data.fats,
+                   #      cal=food_data.calories,
+                   #      pr=food_data.protein,
+                   #      gr=food_data.grams,
+                   #      carbs=food_data.carbs,
+                   #      fats=food_data.fats,
                    #     score=food_data.score,
                )))
         kb.add(InlineKeyboardButton(text="Неправильный подсчет грам",
